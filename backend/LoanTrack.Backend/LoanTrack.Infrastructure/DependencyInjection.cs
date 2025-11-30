19541ae6f0c51e089eb11861ddb166d377819bbf
braulin07 <braulinpg@gmail.com>
2025-11-29 21:46:20 -0400

    Agregar clases para inyeccion de dependencia desde capas Infrastructure y Application

---
+++ backend/LoanTrack.Backend/LoanTrack.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using LoanTrack.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanTrack.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void AddInfrastructureServiceRegistration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("")));
+        }
+    }
+}
