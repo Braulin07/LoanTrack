3ae5019ed01585a08c7fd1c7ef9be682fa9557b2
braulin07 <braulinpg@gmail.com>
2025-11-29 20:45:12 -0400

    Agregar Contexto de la base de datos

---
+++ backend/LoanTrack.Backend/LoanTrack.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using LoanTrack.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanTrack.Infrastructure.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Prestamo> Prestamos { get; set; }
+        public DbSet<Pago> Pagos { get; set; }
+       
+
+    }
+}
---
+++ backend/LoanTrack.Backend/LoanTrack.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using LoanTrack.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanTrack.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public Task<IEnumerable<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+ 
+    }
+}
