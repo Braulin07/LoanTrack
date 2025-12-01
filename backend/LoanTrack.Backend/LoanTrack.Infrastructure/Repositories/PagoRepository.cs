4e8ecc761cca4eab04190bc74a18df656b7cdeae
braulin07 <braulinpg@gmail.com>
2025-11-30 23:16:20 -0400

    Creacion de Repositorios en la capa de Infrastructure

---
+++ backend/LoanTrack.Backend/LoanTrack.Infrastructure/Repositories/PagoRepository.cs
@@ -0,0 +1,38 @@
+﻿using LoanTrack.Domain.Entities;
+using LoanTrack.Domain.Interfaces;
+using LoanTrack.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanTrack.Infrastructure.Repositories
+{
+    public class PagoRepository : GenericRepository<Pago>, IPagoRepository
+    {
+        public PagoRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public Task<IEnumerable<Pago>> GetPagosEntreFechas(DateOnly fechaInicio, DateOnly fechaFin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Pago>> GetPagosPorPrestamo(int idPrestamo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<decimal> GetTotalPagadoPorPrestamo(int idPrestamo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Pago> GetUltimoPago(int idPrestamo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ backend/LoanTrack.Backend/LoanTrack.Infrastructure/Repositories/PrestamoRepository.cs
@@ -0,0 +1,43 @@
+﻿using LoanTrack.Domain.Entities;
+using LoanTrack.Domain.Interfaces;
+using LoanTrack.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanTrack.Infrastructure.Repositories
+{
+    public class PrestamoRepository : GenericRepository<Prestamo>, IPrestamoRepository
+    {
+        public PrestamoRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public Task<decimal> GetMontoTotalPrestamosPorCliente(int idCliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Prestamo>> GetPrestamosActivosPorCliente(int idCliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Prestamo>> GetPrestamosConVencimientoHoy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Prestamo>> GetPrestamosPorEstado(string estado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Prestamo>> GetPrestamosVencidos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
