a4174be3538f1605bf01132e9b6cf6b6020eb32b
braulin07 <braulinpg@gmail.com>
2025-12-09 14:54:11 -0400

    Creacion de Dtos, Interfaza de service y Servicio para Usuario

---
+++ backend/LoanTrack.Backend/LoanTrack.Application/Interfaces/Services/IUsuarioService.cs
@@ -0,0 +1,20 @@
+﻿using LoanTrack.Application.Dtos.Usuario;
+using LoanTrack.Domain.Entities;
+using LoanTrack.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanTrack.Application.Interfaces.Services
+{
+    public interface IUsuarioService
+    {
+        Task<UsuarioReadDto?> GetPorEmailAsync(string email);
+        Task<bool> EmailExisteAsync(string email);
+        Task<IEnumerable<UsuarioReadDto>> GetByRolAsync(RolUsuario rol);
+        Task<UsuarioReadDto?> GetConClienteAsync(int usuarioId);
+        Task ActualizarUltimoLoginAsync(int usuarioId);
+    }
+}
---
+++ backend/LoanTrack.Backend/LoanTrack.Application/Services/UsuarioService.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using LoanTrack.Application.Dtos.Usuario;
+using LoanTrack.Application.Interfaces.Repositories;
+using LoanTrack.Application.Interfaces.Services;
+using LoanTrack.Domain.Entities;
+using LoanTrack.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanTrack.Application.Services
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly IUsuarioRepository _repo;
+        private readonly IMapper _mapper;
+
+        public UsuarioService(IUsuarioRepository repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+
+        public async Task ActualizarUltimoLoginAsync(int usuarioId)
+        {
+            await _repo.ActualizarUltimoLoginAsync(usuarioId);
+        }
+
+        public async Task<bool> EmailExisteAsync(string email)
+        {
+            var entidad = await _repo.EmailExisteAsync(email);
+
+            return true;
+        }
+
+        public async Task<IEnumerable<UsuarioReadDto>> GetByRolAsync(RolUsuario rol)
+        {
+            var entidad = await _repo.GetByRolAsync(rol);
+            var dto = _mapper.Map<IEnumerable<UsuarioReadDto>>(entidad);
+
+            return dto;
+        }
+
+        public async Task<UsuarioReadDto?> GetConClienteAsync(int usuarioId)
+        {
+            var entidad = await _repo.GetConClienteAsync(usuarioId);
+            var dto = _mapper.Map<UsuarioReadDto>(entidad);
+
+            return dto;
+        }
+
+        public async Task<UsuarioReadDto?> GetPorEmailAsync(string email)
+        {
+            var entidad = await _repo.GetPorEmailAsync(email);
+            var dto = _mapper.Map<UsuarioReadDto>(entidad);
+
+            return dto;
+        }
+    }
+}
